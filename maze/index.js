dc5dc2776e26b4c16028a8e4d53df3e105455405
Mohadese <mohade3.2710@gmail.com>
2025-09-06 19:32:40 +0330

    maze: create point function and an empty grid

---
+++ maze/index.js
@@ -0,0 +1,20 @@
+const { Map, List } = Immutable;
+
+const mapWithObjects = Map([
+    [{ x: 3, y: 5 }, 'First Obj'],
+    [{ x: 3, y: 5 }, 'Second Obj'],
+])
+console.log('mapWithObjects length:', mapWithObjects.toArray().length);
+
+const mapWithPoints = Map([
+    [point(3, 5), 'First Obj'],
+    [point(3, 5), 'Second Obj'],
+])
+console.log('mapWithPoints length:', mapWithPoints.toArray().length);
+
+const n = 3;
+const emptyArray = new Array(n ** 2).fill(undefined)
+const roomList = emptyArray.map((_, i) => [point(i % n, Math.floor(i / n)), List()])
+const grid = Map(roomList)
+
+console.log('Grid size:', grid.size);
--- maze/point.js
+++ maze/point.js
@@ -1,15 +1,22 @@
 // we gotta create an immutable data structure
-
 class Point {
-    constructor(x , y) {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
     }
 }
 
+// save the previous points to prevent duplication
 const allPoints = new Map();
 
-export function point(x , y) {
-    const key = ``
-    if(allPoints.has())
+const point = (x, y) => {
+    const key = `${x}-${y}`
+    if (allPoints.has(key)) return allPoints.get(key)
+
+    const newPoint = new Point(x, y)
+    // freezing the object so the properties can't get modified
+    Object.freeze(newPoint)
+
+    allPoints.set(newPoint)
+    return newPoint
 }
