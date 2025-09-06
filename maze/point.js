4f511ff0439f53348d76aa365daa4c5fec2529e0
Mohadese <mohadese@Mohadeses-MacBook-Air.local>
2025-09-06 09:47:12 +0330

    initial commit- add maze js initial file

---
+++ maze/point.js
@@ -0,0 +1,15 @@
+// we gotta create an immutable data structure
+
+class Point {
+    constructor(x , y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+const allPoints = new Map();
+
+export function point(x , y) {
+    const key = ``
+    if(allPoints.has())
+}
