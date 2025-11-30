21851b746c2c4592c5fe6686e042ef7cdec27125
lmohade3l <mohade3.2710@gmail.com>
2025-11-30 23:43:59 +0330

    service-worker: check for offline + initiate a service worker

--- service-worker/web/js/blog.js
+++ service-worker/web/js/blog.js
@@ -2,15 +2,39 @@
 	"use strict";
 
 	var offlineIcon;
+	var isOnline = ("onLine" in navigator) ? navigator.onLine : true
 	var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "");
+	var usingSw = ("serviceWorker" in navigator);
+	var swRegistration;
+	var svcworker;
 
 	document.addEventListener("DOMContentLoaded",ready,false);
-
+	initServiceWorker().catch(console.error);
 
 	// **********************************
 
 	function ready() {
 		offlineIcon = document.getElementById("connectivity-status");
+
+		if(!isOnline) {
+			offlineIcon.classList.remove('hidden')
+		}
+
+		window.addEventListener("online" , function online() {
+			offlineIcon.classList.add('hidden')
+			isOnline = true;
+		})
+
+		window.addEventListener("offline" , function offline() {
+			offlineIcon.classList.remove('hidden')
+			isOnline = false;
+		})
+	}
+
+	async function initServiceWorker() {
+		swRegistration = await navigator.serviceWorker.register("/sw.js" , {
+			updateViaCache: false
+		})
 	}
 
 })();
