a0851d69300373fa501c379006826058372c5bc6
mohadese.gh <mohade3.2710@gmail.com>
2025-12-20 16:45:28 +0330

    Remove nested git repo, add as regular directory

---
+++ react-performance/src/examples/deep-thoughts/components/capture-thought.tsx
@@ -0,0 +1,37 @@
+import { Button } from '$/common/components/button';
+import { Input } from '$/common/components/input';
+import { useState } from 'react';
+
+type CaptureThoughtProps = {
+  onSubmit: (content: string) => void;
+};
+
+export const CaptureThought = ({
+  onSubmit,
+}: CaptureThoughtProps) => {
+  const [draftThought, setDraftThought] = useState('');
+
+  return (
+    <div className="py-4">
+      <form
+        className="flex gap-4"
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit(draftThought);
+        }}
+      >
+        <Input
+          name="thought"
+          placeholder="What's on your mind?"
+          required
+          value={draftThought}
+          onChange={(e) => setDraftThought(e.target.value)}
+        />
+
+        <Button type="submit" className="whitespace-nowrap">
+          Capture Thought
+        </Button>
+      </form>
+    </div>
+  );
+};
---
+++ react-performance/src/examples/effect-essentials/index.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client';
+import Application from './application';
+
+import '$/common/index.css';
+
+const root = document.getElementById('root');
+
+if (root) {
+  createRoot(root).render(<Application />);
+}
